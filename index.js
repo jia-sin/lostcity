e74a4db958c3a6ae6fc7ad08082be73bd4f18fdd
jia-sin <101374326+jia-sin@users.noreply.github.com>
2022-11-10 00:52:10 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let reason = document.getElementById("reason")
+let reasonP = document.getElementById("reason-p")
+let aboutUs = document.getElementById("about-us")
+let aboutUsP = document.getElementById("about-us-p")
+let resultList = document.getElementById("result-list")
+let resultListP = document.getElementById("result-list-p")
+
+
+// reason.addEventListener('click', () => {
+//     // reasonP.scrollTop = reasonP.scrollHeight - reasonP.offsetHeight
+//     reasonP.scrollIntoView()
+// })
+// aboutUs.addEventListener('click', () => {
+//     aboutUsP.scrollIntoView()
+//     // window.scrollTo(0, window.screenY + reason.getBoundingClientRect().bottom)
+// })
+// resultList.addEventListener('click', () => {
+//     resultListP.scrollIntoView()
+//     // window.scrollTo(0, window.screenY + reason.getBoundingClientRect().bottom)
+// })
